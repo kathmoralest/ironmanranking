1f2f682cb5c462e04f87e9f4851d188f9b30415e
kathmoralest <kymorale@espol.edu.ec>
2023-08-07 06:20:27 -0500

    Cleaned content

--- AngularProject/src/app/grafico/grafico.component.ts
+++ AngularProject/src/app/grafico/grafico.component.ts
@@ -23,21 +23,21 @@ export class GraficoComponent {
         reversed: true,
       },
       axisY: {
-        title: "Ganadores IronMan",
+        title: "Campeones",
         interval: 1
       },
       data: [{
         type: "bar",
         dataPoints: [
-          { y: 11, label: "AUS" },
-          { y: 3, label: "BEL" },
-          { y: 8, label: "CAN" },
-          { y: 5, label: "GBR" },
-          { y: 11, label: "GER" },
-          { y: 2, label: "NZL" },
-          { y: 10, label: "SUI" },
-          { y: 28, label: "USA" },
-          { y: 7, label: "ZIM" }
+          { y: 11, label: "🇦🇺"},
+          { y: 3, label: "ᏼꭼꮮ" },
+          { y: 8, label: "ꮯꭺꮑ" },
+          { y: 5, label: "ᏽᏼꭱ" },
+          { y: 11, label: "ᏽꭼꭱ" },
+          { y: 2, label: "ꮑꮓꮮ" },
+          { y: 10, label: "ꮪꮼꮖ" },
+          { y: 28, label: "ꮼꮪꭺ" },
+          { y: 7, label: "ꮓꮖꮇ" }
         ]
       }]
     });
